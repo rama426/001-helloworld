bd04f051417188488ea9d2e68be8681d896f7412
Ramadevi Gavidi <ramadevigavidi0426@gmail.com>
2022-07-06 14:52:57 +0530

    firstcommit

---
+++ src/main/java/com/example/Addition.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Addition {
+public static void main(String[] args) {
+		
+		int a,b,c;
+		a=10;
+		b=11;
+		c=a+b;
+		System.out.println("addition is:"+c);
+		
+		c=a-b;
+		System.out.println("Subtraction is:"+c);
+		
+		c=a*b;
+		System.out.println("Multiplication is:"+c);
+		
+		c=a/b;
+		System.out.println("Division is:"+c);
+}
+}
